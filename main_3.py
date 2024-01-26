79426cfc931d7861520fc4da9f39831f1e7d9974
Seunghan Ryu <jorzor99@gmail.com>
2024-01-27 02:11:40 +0900

    commit

--- main_3.py
+++ main_3.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from enum import Enum
 import joblib
 import pandas as pd
+import uvicorn
 
     
 # 모델 로드
@@ -63,3 +64,5 @@ def predict_loan_limit(application: LoanApplication):
     # 예측 결과 반환
     return {"predicted_loan_limit": prediction[0]}
 
+if __name__ == "__main__":
+    uvicorn.run("main_3:app", host = '0.0.0.0', port=80, reload = True)
